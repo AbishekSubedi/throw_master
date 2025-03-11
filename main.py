f36c74a6d879947e0c3611c2b7b4b941edad70cb
AbishekSubedi <sabitakiran602@gmail.com>
2025-03-11 16:37:13 -0400

    Added a rect obj for player to move on all directions

--- main.py
+++ main.py
@@ -1,20 +1,39 @@
+'''Javeline Throwing Game Made from Pygame'''
+
 import pygame
 
 pygame.init()
-gameDisplay = pygame.display.set_mode((1280, 720))
-pygame.display.set_caption("Throw Master")
-clock = pygame.time.Clock()
-running = True
+SCREEN_WIDTH = 1280
+SCREEN_HEIGHT = 720
+gameDisplay = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
-while running:
+player = pygame.Rect((350, 250, 50, 50))
+
+pygame.display.set_caption("Throw Master")
+
+RUNNING = True
+
+while RUNNING:
+
+    gameDisplay.fill((0, 0, 0))
+
+    pygame.draw.rect(gameDisplay, (255, 0, 0), player)
+
+    key = pygame.key.get_pressed()
+    if key[pygame.K_LEFT]:
+        player.move_ip(-1, 0)
+    elif key[pygame.K_RIGHT]:
+        player.move_ip(1, 0)
+    elif key[pygame.K_UP]:
+        player.move_ip(0, -1)
+    elif key[pygame.K_DOWN]:
+        player.move_ip(0, 1)
+
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            running = False
+            RUNNING = False
 
-    # gameDisplay.fill("Black")
-
-    # pygame.display.flip()
-
-    clock.tick(60)
+    pygame.display.update()
 
 pygame.quit()
